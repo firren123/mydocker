d7f5655daf3c828114e2d42f297cbcb4eadbc012
李晨军 <firren@163.com>
2017-08-09 19:09:01 +0800

    test

--- www/index.php
+++ www/index.php
@@ -6,8 +6,17 @@
  * Date: 17/8/9
  * Time: 上午1:30
  */
-phpinfo();
 class index
 {
 
+    public function aa(){
+        for($n=0;$n<100;$n++){
+            echo "good <br>";
+        }
+    }
 }
+
+$c = new index();
+$c->aa();
+
+    phpinfo();
