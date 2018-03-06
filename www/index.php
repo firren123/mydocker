51aed0f78d6c809a73141a4a94f77b07223184df
李晨军 <lichenjun@iyangpin.com>
2018-03-06 11:50:52 +0800

    xdebug

--- www/index.php
+++ www/index.php
@@ -6,8 +6,20 @@
  * Date: 17/8/9
  * Time: 上午1:30
  */
+//echo 333;exit;
 phpinfo();
 class index
 {
+    public $a = 123;
+    public function aaa(){
+        for($n=1;$n<100;$n++){
+            echo $n."<br>";
+        }
+        echo 44444444;
+    }
 
 }
+$index = new index();
+$index->aaa();
+echo "dddddd";
+exit;
